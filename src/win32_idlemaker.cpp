56e144a7a09b2edfde808e14d9b01ddd108b7735
Antonio <ahmbarreiros.cs@gmail.com>
2023-08-25 22:39:20 -0300

    window size buffer created

--- src/win32_idlemaker.cpp
+++ src/win32_idlemaker.cpp
@@ -1,5 +1,60 @@
 #include <windows.h>
 
+#define  global_variable static
+#define  internal static
+#define  local_variable static
+
+global_variable BOOL Running;
+global_variable void * BitmapMemory;
+global_variable BITMAPINFO BitmapInfo;
+global_variable HDC BitmapDeviceContext;
+global_variable HBITMAP BitmapHandle;
+
+
+internal void Win32ResizeDIBSection(int Width, int Height) {
+
+  if(BitmapHandle) {
+    DeleteObject(BitmapHandle);
+  }
+  
+  if(!BitmapDeviceContext) {
+    BitmapDeviceContext = CreateCompatibleDC(0); 
+  }
+  
+  BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+  BitmapInfo.bmiHeader.biWidth = Width;
+  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biPlanes = 1;
+  BitmapInfo.bmiHeader.biBitCount = 32;
+  BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+  
+  BitmapHandle = CreateDIBSection(
+				  BitmapDeviceContext,
+				  &BitmapInfo,
+				  DIB_RGB_COLORS,
+				  &BitmapMemory,
+				  NULL,
+				  0
+				  );
+}
+
+internal void Win32StretchDIBSection(HDC DeviceContext,
+			    int X,
+			    int Y,
+			    int Width,
+			    int Height) {
+    StretchDIBits(
+		DeviceContext,
+		X, Y, Width, Height,
+		X, Y, Width, Height,
+		BitmapMemory,
+		&BitmapInfo,
+		DIB_RGB_COLORS,
+		SRCCOPY
+		);
+}
+
 
 LRESULT CALLBACK Win32Wndproc(
 			      HWND Window,
@@ -12,7 +67,7 @@ LRESULT CALLBACK Win32Wndproc(
   case WM_CLOSE:
     {
       DestroyWindow(Window);
-      OutputDebugStringA("WM_SIZE\n");
+      OutputDebugStringA("WM_CLOSE\n");
     } break;
   case WM_PAINT:
     {
@@ -25,20 +80,26 @@ LRESULT CALLBACK Win32Wndproc(
       int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;;
       PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
       EndPaint(Window, &Paint);
-      OutputDebugStringA("WM_SIZE\n");     
+      OutputDebugStringA("WM_PAINT\n");     
     } break;
   case WM_SIZE:
     {
+      RECT ClientRect;
+      GetClientRect(Window, &ClientRect);
+      int Width = ClientRect.right - ClientRect.left;
+      int Height = ClientRect.bottom - ClientRect.top;
+      
+      Win32ResizeDIBSection(Width, Height);
       OutputDebugStringA("WM_SIZE\n");
     } break;
   case WM_DESTROY:
     {
       PostQuitMessage(0);
-      OutputDebugStringA("WM_SIZE\n");
+      OutputDebugStringA("WM_DESTROY\n");
     } break;
   default:
     {
-      OutputDebugStringA("WM_SIZE\n");
+      OutputDebugStringA("DEFAULT\n");
       Result = DefWindowProc(Window, Message, wParam, lParam);
     } break;
   }
@@ -75,13 +136,15 @@ int WINAPI WinMain(
 				   0
 				   );
     if(WindowHandle) {
-      for(;;) {
+      Running = true;
+      while(Running) {
 	MSG Message;
 	BOOL bRet = GetMessage(&Message, NULL, 0, 0) != 0;
-	if(bRet != -1) {
+	if(bRet > 0) {
 	  TranslateMessage(&Message);
 	  DispatchMessage(&Message);
 	} else {
+	  Running = 0;
 	  break;
 	}
       }
